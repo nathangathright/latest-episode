35aed79c0ed9e51c31d38eac50b5bbd1d3057c60
Nathan Gathright <nathan.gathright@gmail.com>
2024-03-15 22:33:22 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+let xpath = require("xpath");
+const { DOMParser } = require('@xmldom/xmldom')
+
+const http = require('http');
+const url = require('url');
+const port = 3000;
+
+const server = http.createServer(async (req, res) => {
+    const parsedUrl = url.parse(req.url, true);
+    let { feedURL } = parsedUrl.query;
+    if (!feedURL) {
+        res.statusCode = 400;
+        res.statusMessage = "Bad Request";
+        return;
+    }
+    
+    let response = await fetch(feedURL, {
+        headers: {
+            'User-Agent': 'latest-episode-bot'
+        }
+    });
+
+    let xml = await response.text();
+    let doc = new DOMParser().parseFromString(xml);
+    let nodes = xpath.select("//item/enclosure/@url", doc);
+    let latestEpisode = nodes[0].value;
+
+    res.writeHead(302, {
+        'Location': latestEpisode
+    });
+    res.end();
+});
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
